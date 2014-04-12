ae6cf080e59c6150e319a3c128462c6520f89c48
bollocks <jqrainwater@yahoo.com>
2014-04-12 20:09:27 +0000

    refactor

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,15 +2,21 @@ require 'spec_helper'
 
 describe Item do
   
+  describe "validations" do
+    it "requires name" do
+      user = FactoryGirl.build(:item, name: "")
+      user.should_not be_valid
+      user.errors[:name].should include "can't be blank"
+    end
+    
+    it "requires price" do
+      user = FactoryGirl.build(:item, price: nil)
+      user.should_not be_valid
+      user.errors[:price].should include "can't be blank"
+    end
+  end
+  
   it "has working factory" do
     FactoryGirl.build(:item).should be_valid
-  end
-  
-  it "requires name" do
-    FactoryGirl.build(:item, name: "").should_not be_valid
-  end
-  
-  it "requires price" do
-    FactoryGirl.build(:item, price: nil).should_not be_valid
   end
 end
