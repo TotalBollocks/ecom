8be0b5a0582883cc1932a98d0bc3fc63be01586d
bollocks <jqrainwater@yahoo.com>
2014-04-20 02:21:42 +0000

    Purchase spec

--- spec/features/items/buying_items_spec.rb
+++ spec/features/items/buying_items_spec.rb
@@ -7,8 +7,23 @@ feature "buying items" do
   before do
     sign_in_as user
     visit '/'
-    within("#item.name") do
+    within("##{item.name}") do
       click_link "Buy"
     end
+  end
+  
+  specify {page.should have_content item.name}
+  specify {page.should have_button "Pay #{item.price}"}
+  
+  describe "successful payment" do
+    before do
+      fill_in "Card Number", with: '4242-4242-4242-4242'
+      fill_in "CVC", with: '123'
+      select "January", from: "exp_month"
+      select "2022", from: "exp_year"
+      click_button "Pay #{item.price}"
+    end
+    
+    specify{ page.should have_content "Item has been purchased"}
   end
 end
